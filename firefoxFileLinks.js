1d4846c3f1b8cb481b68cb992a482376c805bb9b
hase <oldm8reggie@gmail.com>
2018-11-02 11:34:55 +1000

    inti commit

---
+++ firefoxFileLinks.js
@@ -0,0 +1,22 @@
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+if (isFirefox) {
+    //You can use what ever to detect links, you can do it by tag and get every link. 
+    var linkmodify = document.getElementsByClassName("list class");
+    for (var i = 0; i < linkmodify.length; i++) {
+        var replacementLink = "";
+        link = linkmodify[i].href;
+        for (var j = 0; j < link.length; j++) {
+            if (j == 5) {
+                replacementLink += '//';
+            }
+            replacementLink += link[j];
+        }
+        linkmodify[i].href = replacementLink;
+    }
+}
